5fa97ba698fd306516345a3c0ca04564cc4cecea
gadkim <70191463+gadkim@users.noreply.github.com>
2020-09-07 20:39:19 +0900

    Update main_prd.tf

--- code/main_prd.tf
+++ code/main_prd.tf
@@ -1,4 +1,15 @@
 # Provider
-provider "aws" { 
-  region     = var.region 
+# provider "aws" {
+#   profile = "gadkim"
+#   region = var.region
+# }
+
+# VPC Resource
+resource "aws_vpc" "main-prd" {
+  cidr_block = "10.51.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "Terraform VPC-PRD"
+  }
 }
